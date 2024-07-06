d3128f50ce6572a7c666677731ae889340e106b0
Daniel Hearn <DanielHearn@users.noreply.github.com>
2024-07-06 18:43:49 +0100

    Remove help

--- app/page.tsx
+++ app/page.tsx
@@ -15,13 +15,8 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-4xl">Prompt Generator</h1>
         <h2 className="text-l max-w-xl">
-          Generate prompts for creative usage such as drawing, 3D modeling or AI
+          Generate prompts for creative usage such as drawing and 3D modeling
         </h2>
-        <h3 className="text-sm max-w-xl italic">
-          Add new words to the prompt and drag words horizontally to reorder them. Click the lock
-          icons to lock individual words to control which get regenerated. Use the dropdown menus to
-          control which type of word is generated.
-        </h3>
       </div>
       <Words />
       <div>
